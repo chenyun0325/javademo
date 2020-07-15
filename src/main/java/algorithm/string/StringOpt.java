b5d218d9409c93ca383b4ab3951dcd31e79f1840
chenyun <chenyun@cai-inc.com>
2020-07-15 22:42:20 +0800

    添加最长回文子串动态规划

--- src/main/java/algorithm/string/StringOpt.java
+++ src/main/java/algorithm/string/StringOpt.java
@@ -18,6 +18,7 @@ public class StringOpt {
         printChildString_v(str.toCharArray(),0);
 
         System.out.println(longestPalindrome("abba"));
+        System.out.println(longestPalindrome1("abba"));
 //
 //        String testStr = "abcdeab";
 //
@@ -232,6 +233,25 @@ public class StringOpt {
         }
         // 返回以begin,end为基准，同时向左向右扩展后能够得到的最长回文串长度
         return right-left-1;
+    }
+
+    /*
+    https://www.cnblogs.com/fsmly/p/10048008.html
+     */
+    static int longestPalindrome1(String str) {
+        int len = str.length();
+        int[][] dp = new int[len][len];
+        for (int i = len - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i+1; j < len; j++) {
+                if(str.charAt(i) == str.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] +2;
+                } else {
+                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[0][len-1];
     }
 
     /**
